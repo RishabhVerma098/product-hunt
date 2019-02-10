bf1bf5c2c1ed41f873b47f7592e072b467623121
rishabh verma <vermarishabh0987@gmail.com>
2019-02-10 23:55:10 +0530

    authentication done

--- account/views.py
+++ account/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 
 def signup(request):
-    return render(request, 'account/signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['confirm_password']:
+            try:
+                # user (can i delete this variable?)
+                User.objects.get(username=request.POST['username'])
+                return render(request, 'account/signup.html', {'error': 'username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], email=request.POST['email'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+
+        else:
+            return render(request, 'account/signup.html', {'error': 'password must match'})
+
+    else:
+        return render(request, 'account/signup.html')
 
 
 def login(request):
-    return render(request, 'account/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(
+            username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'account/login.html', {'error': 'username or password is incorret'})
+    else:
+        return render(request, 'account/login.html')
 
 
 def signout(request):
-    # TODO need to route to homepage
-    return render(request, 'account/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
