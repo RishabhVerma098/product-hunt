429d3da962d6f0da076a5c7446078f8e9a57a04f
rishabh verma <vermarishabh0987@gmail.com>
2019-02-10 20:45:22 +0530

    paritial urls set

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Product-hunt
+
 Django site 2 (focused on Authentication)
 
-## Step 1 ) Setup Database in postgres and create gitignore file 
+## Step 1 ) Setup Database in postgres and create gitignore file
+
+## Step n ) ->
+
+### task 1 ,
+
+send all the traffic to account page for authentication , in main urls.py (use include)
--- account/views.py
+++ account/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def signup(request):
+    return render(request, 'account/signup.html')
+
+
 def login(request):
-    return render(request, 'account/login.html', name='login')
+    return render(request, 'account/login.html')
+
+
+def signout(request):
+    # TODO need to route to homepage
+    return render(request, 'account/signup.html')
